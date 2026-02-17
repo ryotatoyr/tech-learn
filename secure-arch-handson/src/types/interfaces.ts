b7ae9b56e02714ba46ef58f2e2afc9f4119ab9bf
ryotaoyr <ryotaotyr.828@gmail.com>
2026-02-17 17:02:05 +0900

    make interfaces

---
+++ secure-arch-handson/src/types/interfaces.ts
@@ -0,0 +1,20 @@
+export interface User {
+  id: number;
+  name: string;
+  password?: string;
+}
+
+export type UserWithoutPassword = Omit<User, "password">;
+
+export interface IUserRepository {
+  findById(id: number): Promise<User | null>;
+  findByUsername(username: string): Promise<User | null>;
+}
+
+export interface IUserService {
+  getUserById(id: number): Promise<UserWithoutPassword | null>;
+  login(
+    username: string,
+    password: string,
+  ): Promise<UserWithoutPassword | null>;
+}
